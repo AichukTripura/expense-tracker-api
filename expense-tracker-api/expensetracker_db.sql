62f0fb03f27368f483a75e945cf6d9e5d822c103
Aichuk Tripura <living.being123@gmail.com>
2022-03-24 19:26:52 -0400

    Initialized project

---
+++ expense-tracker-api/expensetracker_db.sql
@@ -0,0 +1,15 @@
+drop database expensetrackerdb;
+drop user expensetracker;
+create user expensetracker with password 'password';
+create database expensetrackerdb with template=template0 owner=expensetracker;
+\connect expensetrackerdb;
+alter default privileges grant all on tables to expensetracker;
+alter default privileges grant all on sequences to expensetracker;
+
+create table et_users(
+user_id integer primary key not null,
+first_name varchar(20) not null,
+last_name varchar(20) not null,
+email varchar(30) not null,
+password text not null
+)
